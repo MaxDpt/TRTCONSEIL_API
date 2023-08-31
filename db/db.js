fa5d52e411a1710f1548d698de8b4fbbb4d46b08
Maxdpt <86427948+MaxDpt@users.noreply.github.com>
2023-08-31 10:40:13 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -7,6 +7,7 @@ const result = dotenv.config();
 
 // paramètre de connexion à la base de donnée
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
