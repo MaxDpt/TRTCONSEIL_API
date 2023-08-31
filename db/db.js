4a825a279998f1f521c4c7ee29c57ef9913dea1a
Maxdpt <86427948+MaxDpt@users.noreply.github.com>
2023-08-31 10:54:05 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,14 +1,18 @@
 // import de l'outil Pool de Express pg pour connexion à la base de donnée
-const pool = require('pg')
+const Pool = require('pg').Pool
 
 // import package pour utiliser les variables d'environnment
 const dotenv = require("dotenv");
 const result = dotenv.config();
 
-
+// paramètre de connexion à la base de donnée
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 // connexion à la base de donnée
-pool.connect(process.env.DATABASE_URL)
+pool.connect()
 .then(() => console.log("connexion à la base de donnée réussi"))
 .catch(() => console.log("connexion à la base de donnée échoué"))
 
